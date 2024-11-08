94e27f38d15014be45c8992b7a6780106049340c
Beatryzgurgel <annabeatryzgomesgurgel@gmail.com>
2024-11-08 10:46:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,76 +1,50 @@
-function mostrarMenu() {
-    let opcao;
-    
-    do {
-        opcao = prompt(`Escolha uma opção:
-1 - Área do triângulo
-2 - Área do retângulo
-3 - Área do quadrado
-4 - Área do trapézio
-5 - Área do círculo
-6 - Sair`);
+const properties = []
+let option
 
-        let area;
-        
-        switch(opcao) {
-            case "1":
-                area = calcularAreaTriangulo()
-                alert(`A área do triângulo é: ${area}`)
-                break
-            case "2":
-                area = calcularAreaRetangulo();
-                alert(`A área do retângulo é: ${area}`);
-                break;
-            case "3":
-                area = calcularAreaQuadrado();
-                alert(`A área do quadrado é: ${area}`);
-                break;
-            case "4":
-                area = calcularAreaTrapezio();
-                alert(`A área do trapézio é: ${area}`);
-                break;
-            case "5":
-                area = calcularAreaCirculo();
-                alert(`A área do círculo é: ${area}`);
-                break;
-            case "6":
-                alert("Saindo...");
-                break;
-            default:
-                alert("Opção inválida. Tente novamente.");
-                break;
-        }
-    } while (opcao !== "6");
-}
+do {
+    option = prompt(
+        `Bem vindo ao cadastro de Imóveis:\nQuantidade de imóveis ja cadastrados: ${properties.length}\n\n1. Adcionar novo imóvel\n2. Lista de imóveis salvos\n3. Sair do cadastro`
+    )
 
-function calcularAreaTriangulo() {
-    let base = parseFloat(prompt("Informe a base do triângulo:"));
-    let altura = parseFloat(prompt("Informe a altura do triângulo:"));
-    return (base * altura) / 2;
-}
+    switch (option) {
+        case '1':
+            const property = {}
 
-function calcularAreaRetangulo() {
-    let base = parseFloat(prompt("Informe a base do retângulo:"));
-    let altura = parseFloat(prompt("Informe a altura do retângulo:"));
-    return base * altura;
-}
+            property.owner = prompt("Digite o nome do proprietário:")
+            property.bedrooms = parseInt(prompt("Digite a quantidade de quartos que deseja:"))
+            property.bathrooms = parseInt(prompt("Digite a quantidade de banheiros que deseja:"))
+            property.garage = confirm("O imóvel tem garagem?")
 
-function calcularAreaQuadrado() {
-    let lado = parseFloat(prompt("Informe o lado do quadrado:"));
-    return lado * lado;
-}
+            const confirmSave = confirm(
+                `Deseja salvar este imóvel?\n\nProprietário: ${property.owner}\nQuartos: ${property.bedrooms}\nBanheiros: ${property.bathrooms}\nGaragem: ${property.garage ? "Sim" : "Não"}`
+            )
 
-function calcularAreaTrapezio() {
-    let baseMaior = parseFloat(prompt("Informe a base maior do trapézio:"));
-    let baseMenor = parseFloat(prompt("Informe a base menor do trapézio:"));
-    let altura = parseFloat(prompt("Informe a altura do trapézio:"));
-    return ((baseMaior + baseMenor) * altura) / 2;
-}
+            if (confirmSave) {
+                properties.push(property)
+                alert("Imóvel adcionado com sucesso!")
+            } else {
+                alert("Imóvel descartado.")
+            }
+            break
 
-function calcularAreaCirculo() {
-    let raio = parseFloat(prompt("Informe o raio do círculo:"));
-    let pi = 3.14;
-    return pi * raio * raio;
-}
+            case '2':
+                if (properties.length === 0) {
+                    alert("Nenhum imóvel cadastrado foi encontrado.")
+                } else {
+                    let propertyList = "Lista de Imóveis ja Cadastrados:\n\n"
+                    for (let i = 0; i < properties.length; i++) {
+                        propertyList += `Imóvel ${i + 1}:\n nome do Proprietário: ${properties[i].owner}\n Quantidade de Quartos: ${properties[i].bedrooms}\n Quantidade de Banheiros: ${properties[i].bathrooms}\nGaragem: ${properties[i].garage ? "Sim" : "Não"}\n\n`
+                    }
+                    alert(propertyList)
+                }
+                break            
 
-mostrarMenu();
+        case '3':
+            alert("Finalizando o programa.")
+            break
+
+        default:
+            alert("Opção não existente.! Tente novamente.")
+            break
+    }
+} while (option !== '3')
